4678a06b5ecf8c8d5f1a9f39b77bb06f8bc74523
Tim Peter <tim.peter@eonum.ch>
2017-12-06 14:48:15 +0100

    Load and use the supplement grouper.

--- src/main/java/ch/eonum/grouperserve/GrouperServe.java
+++ src/main/java/ch/eonum/grouperserve/GrouperServe.java
@@ -16,13 +16,17 @@ import org.slf4j.LoggerFactory;
 import org.swissdrg.grouper.EffectiveCostWeight;
 import org.swissdrg.grouper.GrouperResult;
 import org.swissdrg.grouper.IGrouperKernel;
+import org.swissdrg.grouper.IGrouperKernel.Tariff;
 import org.swissdrg.grouper.IPatientCaseParser;
 import org.swissdrg.grouper.PatientCase;
 import org.swissdrg.grouper.PatientCaseParserFactory;
 import org.swissdrg.grouper.PatientCaseParserFactory.InputFormat;
 import org.swissdrg.grouper.WeightingRelation;
+import org.swissdrg.zegrouper.api.ISupplementGroupResult;
+import org.swissdrg.zegrouper.api.ISupplementGrouper;
 import org.swissdrg.grouper.Catalogue;
 import org.swissdrg.grouper.SpecificationReader;
+import org.swissdrg.grouper.SupplementPatientCase;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,8 +41,9 @@ public class GrouperServe {
 	private static final int INTERNAL_SERVER_ERROR = 500;
 	private static final String GROUPERSPECS_FOLDER = "grouperspecs/";
 	private static HashMap<String, IGrouperKernel> grouperKernels;
+	private static HashMap<String, ISupplementGrouper> zeKernels;
 	private static HashMap<String, Map<String, WeightingRelation>> catalogues;
-	private static IPatientCaseParser pcParser = PatientCaseParserFactory.getParserFor(InputFormat.URL);
+	private static IPatientCaseParser pcParser = PatientCaseParserFactory.getParserFor(InputFormat.URL, Tariff.SWISSDRG);
 	
 	public static void main(String[] args) {
 		String systems = loadSystems();
@@ -72,6 +77,7 @@ public class GrouperServe {
 
         	boolean prettyPrint = "true".equals(request.queryParams("pretty"));
         	boolean annotate = "true".equals(request.queryParams("annotate"));
+        	boolean zeGroup = "true".equals(request.queryParams("zegroup"));
         		
         	
         	String version = request.queryParams("version");
@@ -85,6 +91,16 @@ public class GrouperServe {
         	result.put("effectiveCostWeight", ecw);
         	if(annotate)
         		result.put("patientCase", pc);
+        	
+        	if(zeGroup) {
+        		if (!zeKernels.containsKey(version)) {
+        			response.status(HTTP_BAD_REQUEST);
+                    return "There is no supplement grouper for system " + version;
+        		}
+        		SupplementPatientCase sPc = new SupplementPatientCase(pc);      
+                ISupplementGroupResult zeResult = zeKernels.get(version).group(sPc);
+        		result.put("zeResult", zeResult);
+        	}
         	
         	response.status(200);
             response.type("application/json");
@@ -182,6 +198,7 @@ public class GrouperServe {
 			
 			grouperKernels = new HashMap<>();
 			catalogues = new HashMap<>();
+			zeKernels = new HashMap<>();
 			
 			SpecificationReader reader = new SpecificationReader();
 			
@@ -190,6 +207,8 @@ public class GrouperServe {
 				String version = system.get("version");
 				log.info("Loading grouper " + version);
 				String workspace = GROUPERSPECS_FOLDER + version + "/";
+				
+				/** Load DRG catalogue. */
 				try {
 					catalogues.put(version, Catalogue.createFrom( workspace + "catalogue-acute.csv"));
 				} catch (FileNotFoundException e) {
@@ -197,11 +216,27 @@ public class GrouperServe {
 							+ workspace + "catalogue-acute.csv");
 					stop();
 				}
+				
+				/** Load DRG logic from JSON workspace. */
 				try {
 					IGrouperKernel grouper = reader.loadGrouper(workspace);
 					grouperKernels.put(version, grouper);
 				} catch (Exception e) {
 					log.error("Error while loading DRG workspace " + workspace);
+					log.error(e.getMessage());
+					e.printStackTrace();
+					stop();
+				}
+				
+				/** Load supplement grouper (Zusatzentgeltgrouper) logic from JSON. */
+				try {
+					ISupplementGrouper supplementGrouper = reader.loadSupplementGrouper(workspace);
+					zeKernels.put(version, supplementGrouper);
+					log.info("Loaded ZE Grouper " + version);
+				} catch (IOException fe) {
+					// do nothing
+				} catch (Exception e) {
+					log.error("Error while loading ZE grouper " + workspace + "ze-logic.json");
 					log.error(e.getMessage());
 					e.printStackTrace();
 					stop();
