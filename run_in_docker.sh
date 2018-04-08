29d5c2c1c5489b9a0fb6c47af588888ec33bffe3
Tim Peter <tim.peter@eonum.ch>
2018-04-08 19:43:00 +0200

    Update docker command to 0.2.0

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD:/opt/grouperserve -p 4567:4567 --workdir /opt/grouperserve --name=grouperserve --detach=true openjdk:8 java -cp build/libs/grouperserve-0.1.1.jar ch.eonum.grouperserve.GrouperServe
+docker run -v $PWD:/opt/grouperserve -p 4567:4567 --workdir /opt/grouperserve --name=grouperserve --detach=true openjdk:8 java -cp build/libs/grouperserve-0.2.0.jar ch.eonum.grouperserve.GrouperServe
